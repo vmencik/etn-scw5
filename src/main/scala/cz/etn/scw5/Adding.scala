71f3d43d33c049da2634052ec3a343cff27a1023
Vlastimil Menčík <vlastimil.mencik@etnetera.cz>
2013-05-19 10:21:09 +0200

    Finished with first example

--- src/main/scala/cz/etn/scw5/Adding.scala
+++ src/main/scala/cz/etn/scw5/Adding.scala
@@ -7,8 +7,10 @@ import akka.actor.Props
 object Adding extends App {
 
   class AddingActor(add: Int, delay: Int) extends Actor {
+    println("Instantiating")
 
     def receive = {
+      case 3 => throw new RuntimeException("Error 3")
       case n: Int =>
         Thread.sleep(delay * 1000)
         println(Thread.currentThread.getName)
@@ -19,15 +21,23 @@ object Adding extends App {
       case _ => println("received unknown message")
     }
 
+    override def postRestart(e: Throwable) = {
+      println(e.getMessage)
+    }
+
   }
 
   val system = ActorSystem("workshop")
-  val adder = system.actorOf(Props(new AddingActor(10, 1)))
-  val secondAdder = system.actorOf(Props(new AddingActor(999, 2)))
+  val adder = system.actorOf(Props(new AddingActor(10, 1)), "first")
+  val secondAdder = system.actorOf(Props(new AddingActor(999, 2)), "second")
+  println(adder)
+  println(secondAdder)
 
   println(Thread.currentThread.getName)
-  adder ! 255
-  secondAdder ! 8888
+  adder ! 3
+  adder ! 5
+  println("sent messages")
+  //secondAdder ! 8888
 
   Thread.sleep(2500)
   system.shutdown()
--- src/main/scala/cz/etn/scw5/Dummy.scala
+++
@@ -1,3 +0,0 @@
-package cz.etn.scw5
-
-class Dummy
