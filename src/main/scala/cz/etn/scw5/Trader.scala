02826dfdcea9204dfe96eff6968a6f1e1e564042
Vlastimil Menčík <vlastimil.mencik@etnetera.cz>
2013-06-16 11:05:11 +0200

    Subscribe v traderu.

--- src/main/scala/cz/etn/scw5/Trader.scala
+++ src/main/scala/cz/etn/scw5/Trader.scala
@@ -4,10 +4,15 @@ import akka.actor.Actor
 import akka.actor.ActorRef
 
 class Trader(exchange: ActorRef) extends Actor {
-  
+
   def receive = {
     case t: Trade =>
       println(self.path.name + ": " + t)
+  }
+
+  // Po inicializaci konstruktorem. Volano i po restartu.
+  override def preStart() {
+    exchange ! Subscribe(self)
   }
 
 }
--- src/test/scala/cz/etn/scw5/TraderSuite.scala
+++ src/test/scala/cz/etn/scw5/TraderSuite.scala
@@ -1,5 +1,11 @@
 package cz.etn.scw5
 
-class TraderSuite extends AkkaSuite {
+import akka.testkit.TestActorRef
+import akka.actor.Props
 
+class TraderSuite extends AkkaSuite {
+  "Trader" should "subscribe to an exchange once created" in {
+    val trader = TestActorRef(Props(new Trader(testActor)))
+    expectMsg(Subscribe(trader))
+  }
 }
