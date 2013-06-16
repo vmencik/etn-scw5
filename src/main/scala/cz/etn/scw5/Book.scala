18bc8b4e97e2160dac47190c89aa38a881d46584
Vlastimil Menčík <vlastimil.mencik@etnetera.cz>
2013-06-16 12:34:46 +0200

    Zauctovani prodeje

--- src/main/scala/cz/etn/scw5/Book.scala
+++ src/main/scala/cz/etn/scw5/Book.scala
@@ -2,15 +2,24 @@ package cz.etn.scw5
 
 import akka.actor.Actor
 
-class Book(initCash: Int) extends Actor {
+class InsufficientFundsException extends RuntimeException {
+
+}
+
+class Book(initCash: Int, initCommodities: Map[String, Int] = Map.empty) extends Actor {
 
   private var _cash = initCash
-  private var _commodities = Map[String, Int]().withDefaultValue(0)
+  private var _commodities = initCommodities.withDefaultValue(0)
 
   def receive = {
     case Buy(commodity, quantity, price) =>
-      _cash -= quantity * price
+      val diff = quantity * price
+      if (_cash < diff) throw new InsufficientFundsException
+      _cash -= diff
       _commodities = _commodities updated (commodity, _commodities(commodity) + quantity)
+    case Sell(commodity, quantity, price) =>
+      _cash += quantity * price
+      _commodities = _commodities updated (commodity, _commodities(commodity) - quantity)
   }
 
   def cash: Int = _cash
--- src/test/scala/cz/etn/scw5/BookSuite.scala
+++ src/test/scala/cz/etn/scw5/BookSuite.scala
@@ -16,4 +16,22 @@ class BookSuite extends AkkaSuite {
 
   }
 
+  it should "record sell of gold" in {
+
+    val book = TestActorRef[Book](Props(new Book(initCash = 1500, initCommodities = Map("gold" -> 20))))
+
+    book ! Sell("gold", quantity = 10, price = 50)
+
+    book.underlyingActor.cash should be(2000)
+    book.underlyingActor.commodities should be(Map("gold" -> 10))
+  }
+
+  it should "produce exception when cash is insufficient" in {
+
+    val book = TestActorRef[Book](Props(new Book(initCash = 100)))
+    evaluating {
+      book.underlyingActor.receive(Buy("gold", quantity = 10, price = 50))
+    } should produce[InsufficientFundsException]
+  }
+
 }
