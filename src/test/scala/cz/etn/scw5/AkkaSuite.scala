9b1450c9db923eb671e28406e6a478d9d667b3ff
Vlastimil Menčík <vlastimil.mencik@etnetera.cz>
2013-10-13 09:11:01 +0200

    kompatibilita se Scalatest 2.0.RC1

--- src/test/scala/cz/etn/scw5/AkkaSuite.scala
+++ src/test/scala/cz/etn/scw5/AkkaSuite.scala
@@ -4,8 +4,10 @@ import akka.testkit.TestKit
 import akka.actor.ActorSystem
 import org.scalatest.matchers.ShouldMatchers
 import org.scalatest.FlatSpec
+import org.scalatest.FlatSpecLike
+import org.scalatest.Matchers
 
 class AkkaSuite extends TestKit(ActorSystem("exchange"))
-  with FlatSpec
-  with ShouldMatchers
+  with FlatSpecLike
+  with Matchers
   with ShutdownAfterAll
